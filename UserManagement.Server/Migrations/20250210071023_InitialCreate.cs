877c4025cf7c1a4326bb712eb9b4d6f3bee90ba2
Hackandres99 <andres19saavedra9castro9@gmail.com>
2025-02-10 02:14:14 -0500

    Adding initial migrations

---
+++ UserManagement.Server/Migrations/20250210071023_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newsequentialid())"),
+                    firstname = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    lastname = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    accountPassword = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    avatarImagePath = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    creationDate = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())"),
+                    updateDate = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__tmp_ms_x__3214EC07A7A70827", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UK_email",
+                table: "user",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UK_username",
+                table: "user",
+                column: "username",
+                unique: true,
+                filter: "[username] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
